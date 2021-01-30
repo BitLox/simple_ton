f95cce8fd7ab888b9c97e1c534d3da6c5af94c4b
bitlox <dana.coe@bitlox.com>
2021-01-30 08:33:06 +0000

    Iterate through the n of m when creating wallets

--- wallet.sh
+++ wallet.sh
@@ -31,21 +31,29 @@ else
   fi
   if [ "$os_type" == 'OSX' ]
     then
-      wget https://github.com/BitLox/tonos-cli/releases/download/v0.1.29/tonos-cli_v0.1.29_darwin.tar.gz
+      # wget https://github.com/BitLox/tonos-cli/releases/download/v0.1.29/tonos-cli_v0.1.29_darwin.tar.gz
+      wget http://localhost/tonos-cli_v0.1.29_darwin.tar.gz
       mkdir ./tonos-cli
       tar -xvf tonos-cli_v0.1.29_darwin.tar.gz -C ./tonos-cli
       rm tonos-cli_v0.1.29_darwin.tar.gz
   fi
   cd ./tonos-cli
-  wget https://github.com/tonlabs/ton-labs-contracts/raw/master/solidity/safemultisig/SafeMultisigWallet.abi.json
-  wget https://github.com/tonlabs/ton-labs-contracts/raw/master/solidity/safemultisig/SafeMultisigWallet.tvc
+  # wget https://github.com/tonlabs/ton-labs-contracts/raw/master/solidity/safemultisig/SafeMultisigWallet.abi.json
+  # wget https://github.com/tonlabs/ton-labs-contracts/raw/master/solidity/safemultisig/SafeMultisigWallet.tvc
+  wget http://localhost/SafeMultisigWallet.abi.json
+  wget http://localhost/SafeMultisigWallet.tvc
   #Network configuration
   # echo "Choose network, main or dev: "
   # read networkInput
   # network=$networkInput
   
-#Creating a folder for keys
-mkdir data
+  read -p "N of M wallet - Enter N :  " nValue
+  clear
+  read -p "N of M wallet - Enter M :  " mValue
+  clear
+  
+  echo $nValue > N.txt
+  echo $mValue > M.txt
   
   
 #While & Case
@@ -76,92 +84,106 @@ read -n 1 line
 done
 clear
 
-#While & Case
-while [ $? -ne 1 ]
+
+
+  
+./tonos-cli config --url https://$network.ton.dev --wc $workchain >> log_step1.txt
+
+max=$mValue
+for (( i=1; i < max+1; i++ ))
 do
-        phraseMenu
-        case $optionPhrase in
-1)
-        setGenPhrase ;;
-2)
-        setCustomPhrase ;;
-esac
-read -n 1 line
-done
-clear
-
-# read -p "N of M wallet - Enter N :  " nValue
-# clear
-# read -p "N of M wallet - Enter M :  " mValue
-#   
+  loopCount=$i
+  #Creating a folder for keys
+  eval "mkdir -p ${i}_data"
+  
+  #While & Case
+  while [ $? -ne 1 ]
+  do
+          phraseMenu
+          case $optionPhrase in
+  1)
+          setGenPhrase ;;
+  2)
+          setCustomPhrase ;;
+  esac
+  read -n 1 line
+  done
+  clear
   
   
-./tonos-cli config --url https://$network.ton.dev >> log_step1.txt
-./tonos-cli config --wc $workchain >> log_step1.txt
-  
-
   #Creating a temporary file for generating public key
-  phrase=$(cat data/phrase.txt)
+  phrase=$(cat ${loopCount}_data/phrase.txt)
+  
+  # echo $phrase
+  # phrase=$(cat ${i}_data/phrase.txt)
   pubkey="./tonos-cli genpubkey ${phrase}"
   echo $pubkey > pubkey.tmp.sh
   #Generating public key (3.1.2)
   chmod +x pubkey.tmp.sh
-  ./pubkey.tmp.sh > data/pubkey.txt
+  ./pubkey.tmp.sh > ${loopCount}_data/pubkey.txt
   rm pubkey.tmp.sh
   #Sending a response to the log
-  cat data/pubkey.txt >> log_step1.txt
+  cat ${loopCount}_data/pubkey.txt >> log_step1.txt
   #Removing junk data from public key response
   if [ "$os_type" == 'LINUX' ] 
   then
-    sed -i -e '/Public*/! d' -e 's/P.* //' data/pubkey.txt
+    sed -i -e '/Public*/! d' -e 's/P.* //' ${loopCount}_data/pubkey.txt
   fi
   if [ "$os_type" == 'OSX' ]
   then
-    sed '/Public*/! d' data/pubkey.txt > data/pubkey.tmp.txt
-    sed 's/P.* //' data/pubkey.tmp.txt > data/pubkey.txt
-    rm data/pubkey.tmp.txt
+    sed '/Public*/! d' ${loopCount}_data/pubkey.txt > ${loopCount}_data/pubkey.tmp.txt
+    sed 's/P.* //' ${loopCount}_data/pubkey.tmp.txt > ${loopCount}_data/pubkey.txt
+    rm ${loopCount}_data/pubkey.tmp.txt
   fi
 
   #Generating deployment key pair (3.2.1)
-  phrase=$(cat data/phrase.txt)
-  keypair="./tonos-cli getkeypair deploy.keys.json ${phrase}"
+  phrase=$(cat ${loopCount}_data/phrase.txt)
+  keypair="./tonos-cli getkeypair ${loopCount}_data/deploy.keys.json ${phrase}"
   echo $keypair > keypair.tmp.sh
   chmod +x keypair.tmp.sh
   ./keypair.tmp.sh >> log_step1.txt
   rm keypair.tmp.sh
   #Generating multisignature wallet address (Raw address)
-  ./tonos-cli genaddr SafeMultisigWallet.tvc SafeMultisigWallet.abi.json --setkey deploy.keys.json --wc $workchain > data/rawaddr.txt
+  ./tonos-cli genaddr SafeMultisigWallet.tvc SafeMultisigWallet.abi.json --setkey ${loopCount}_data/deploy.keys.json --wc $workchain > ${loopCount}_data/rawaddr.txt
   #Sending a response to the log
-  cat data/rawaddr.txt >> log_step1.txt
+  cat ${loopCount}_data/rawaddr.txt >> log_step1.txt
   #Removing junk data from raw address response
   if [ "$os_type" == 'LINUX' ] 
   then
-    sed -i -e '/Raw*/! d' -e 's/R.* //' data/rawaddr.txt
+    sed -i -e '/Raw*/! d' -e 's/R.* //' ${loopCount}_data/rawaddr.txt
   fi
   if [ "$os_type" == 'OSX' ]
   then
-    sed '/Raw*/! d' data/rawaddr.txt > data/rawaddr.tmp.txt
-    sed 's/R.* //' data/rawaddr.tmp.txt > data/rawaddr.txt
-    rm data/rawaddr.tmp.txt
+    sed '/Raw*/! d' ${loopCount}_data/rawaddr.txt > ${loopCount}_data/rawaddr.tmp.txt
+    sed 's/R.* //' ${loopCount}_data/rawaddr.tmp.txt > ${loopCount}_data/rawaddr.txt
+    rm ${loopCount}_data/rawaddr.tmp.txt
   fi
   
   #Link to ton.live account
-  rawaddr=$(cat data/rawaddr.txt)
+  rawaddr=$(cat ${loopCount}_data/rawaddr.txt)
   tonlive="https://${network}.ton.live/accounts?section=details&id=${rawaddr}"
-  echo $tonlive > account.link.txt
-  mkdir to_ton-keys_folder
-  cp ./data/rawaddr.txt ./to_ton-keys_folder/hostname.addr
-  cp ./deploy.keys.json ./to_ton-keys_folder/msig.keys.json
-  mkdir ./SWData
-  cd ..
-  clear
-  echo -e "Succeeded\n\nAll keys: ./data\n\nLogs: ./log_step1.txt\n\nTo check the log:\ncat log_step1.txt\n\nLink to your account (${network}.ton.live): ./account.link.txt\n\nSend some tokens to your address:\n${rawaddr}\n\nThen Step2.sh"
+  echo $tonlive > ${loopCount}_data/account.link.txt
+  mkdir ${loopCount}_data/to_ton-keys_folder
+  cp ./${loopCount}_data/rawaddr.txt ./${loopCount}_data/to_ton-keys_folder/hostname.addr
+  cp ./${loopCount}_data/deploy.keys.json ./${loopCount}_data/to_ton-keys_folder/msig.keys.json
+  mkdir ./${loopCount}_data/SWData
+  # cd ..
+  echo `pwd`
+  # clear
+done
+baseaddr=$(cat 1_data/rawaddr.txt)
+echo -e "Succeeded\n\nAll keys: ./data\n\nLogs: ./log_step1.txt\n\nTo check the log:\ncat log_step1.txt\n\nLink to your account (${network}.ton.live): ./account.link.txt\n\nSend some 1-2 tokens to your base address:\n${baseaddr}\n\nThen Step2.sh"
+
+cd ..
 fi
 }
+ 
 
-function  checkbalance {
+
+function  checkbalance() {
 clear
-cd ./tonos-cli
+# echo `pwd`
+cd ./tonos-cli/${loopCount}_data
 file="GetBalance.sh"
 
 if [ -f ${file} ]
@@ -170,7 +192,7 @@ then
   if [ "$os_type" == 'LINUX' ] 
   then
     sed -e '/bal/!d' -e 's/bal.*:       //' SWData/lastbalance.txt
-    cd ..
+    cd ../..
   fi
   if [ "$os_type" == 'OSX' ]
     then
@@ -178,46 +200,45 @@ then
     sed 's/bal.*:       //' SWData/lastbalance.tmp.txt > SWData/lastbalance.txt
     rm SWData/lastbalance.tmp.txt
     cat SWData/lastbalance.txt
-    cd ..
+    cd ../..
   fi
 else
-  rawaddr=$(cat data/rawaddr.txt)
-  balance="./tonos-cli account ${rawaddr}"
+  rawaddr=$(cat rawaddr.txt)
+  balance=".././tonos-cli account ${rawaddr}"
   echo $balance > GetBalance.sh
   chmod +x GetBalance.sh
   clear
-  cd .. 
+  cd ../.. 
   ./wallet.sh
 fi
 }
 
-function  showaddress {
+function  showaddress() {
   clear
-  cd ./tonos-cli
-  cat data/rawaddr.txt 
-  cd ..
+  cat ./tonos-cli/${i}_data/rawaddr.txt 
 }
 
-function setGenPhrase {
+function setGenPhrase() {
 #Generating seed phrase (3.1.1)
-./tonos-cli genphrase > data/phrase.txt
+# echo "in setGenPhrase"
+./tonos-cli genphrase > ${loopCount}_data/phrase.txt
 #Sending a response to the log
-cat data/phrase.txt >> log_step1.txt
+cat ${loopCount}_data/phrase.txt >> log_step1.txt
 #Ydalenie lishnego iz frazu
 if [ "$os_type" == 'LINUX' ] 
 then
-  sed -i -e 's/[^"]*//' -e '/^$/d' data/phrase.txt
+  sed -i -e 's/[^"]*//' -e '/^$/d' ${loopCount}_data/phrase.txt
 fi
 if [ "$os_type" == 'OSX' ]
 then
-sed 's/[^"]*//' data/phrase.txt > data/phrase.tmp.txt
-  tr -d '\n' <    data/phrase.tmp.txt > data/phrase.txt 
-  rm data/phrase.tmp.txt
+sed 's/[^"]*//' ${loopCount}_data/phrase.txt > ${loopCount}_data/phrase.tmp.txt
+  tr -d '\n' <    ${loopCount}_data/phrase.tmp.txt > ${loopCount}_data/phrase.txt 
+  rm ${loopCount}_data/phrase.tmp.txt
 fi
 break;
 }
 
-function setCustomPhrase {
+function setCustomPhrase() {
   echo
 echo -e "\tSecret phrase:"  
   read inputPhrase
@@ -229,13 +250,13 @@ echo -e "\tSecret phrase:"
 
 function setMain {
   network="MAIN";
-  echo "MAIN" > data/network.txt
+  echo "MAIN" > network.txt
   break;
 }
 
 function setDev {
   network="NET";
-  echo "NET" > data/network.txt
+  echo "NET" > network.txt
   break;
 }
 
@@ -261,17 +282,6 @@ cd ./tonos-cli
 
 pubkey=$(cat data/pubkey.txt)
 
-read -p "N of M wallet - Enter N :  " nValue
-clear
-read -p "N of M wallet - Enter M :  " mValue
-clear  
-read -p "cosigner 1 (no 0x):  " cosigner1Pubkey
-clear
-read -p "cosigner 2 (no 0x):  " cosigner2Pubkey
-clear
-echo -e "Deploying..." 
-
-
 deploy="./tonos-cli deploy SafeMultisigWallet.tvc '{\"owners\":[\"0x${pubkey}\",\"0x${cosigner1Pubkey}\",\"0x${cosigner2Pubkey}\"],\"reqConfirms\":${nValue}}' --abi SafeMultisigWallet.abi.json --sign deploy.keys.json "
 echo $deploy  > deploy.tmp.sh
 chmod +x deploy.tmp.sh
@@ -284,13 +294,6 @@ echo $status > StatCheck.sh
 chmod +x StatCheck.sh
 ./StatCheck.sh >> log_step2.txt
 
-#Creating transaction online (5 tokens to address 0:2fa8e77ea0855ce446bd60e22035a48d484f55fc05e669661f16f8fb063beacb)
-# phrase=$(cat data/phrase.txt)
-# trans="./tonos-cli call ${rawaddr} submitTransaction '{\"dest\":\"0:2fa8e77ea0855ce446bd60e22035a48d484f55fc05e669661f16f8fb063beacb\",\"value\":5000000000,\"bounce\":false,\"allBalance\":false,\"payload\":\"\"}' --abi SafeMultisigWallet.abi.json --sign ${phrase}"
-# echo $trans > trans.tmp.sh
-# chmod +x trans.tmp.sh
-# ./trans.tmp.sh >> log_step2.txt
-# rm trans.tmp.sh
 
 #Requesting the list of custodian public keys from the blockchain
 custocheck="./tonos-cli run ${rawaddr} getCustodians {} --abi SafeMultisigWallet.abi.json"
@@ -306,7 +309,7 @@ cd ..
 
 function phraseMenu {
   clear
-  
+  echo `pwd`
   echo -e "\tSecret phrase: " 
   echo
   echo -e "\t1. Auto-generate phrase"
@@ -317,7 +320,7 @@ function phraseMenu {
 
 function networkMenu {
   clear
-  
+  echo `pwd`
   echo -e "\tNetwork type: " 
   echo
   echo -e "\t1. MAIN"
@@ -328,7 +331,7 @@ function networkMenu {
 
 function wcMenu {
   clear
-  
+  echo `pwd`
   echo -e "\tWork Chain choice: " 
   echo
   echo -e "\t1. 0"
